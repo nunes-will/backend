d29a5ef0b44261250660ff4e2cdd2886af773a9a
nunes-will <william-x7@hotmail.com>
2020-03-26 10:53:13 -0300

    Added all the functionalities to backend

--- src/database/migrations/20200324115139_create_incidents.js
+++ src/database/migrations/20200324115139_create_incidents.js
@@ -4,7 +4,7 @@ exports.up = function(knex) {
         table.increments();
         table.string('title').notNullable();
         table.string('description').notNullable();
-        table.decimal('valie').notNullable();
+        table.decimal('value').notNullable();
 
         table.string('ong_id').notNullable();
 
